1643d0563bc45762dba3edf1f544e1783da586a7
Eryk Pabianczyk <erykmcl@gmail.com>
2021-03-10 15:33:26 +0000

    removed files that were moved in previous commit

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, request
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-
-
-class ReadItemTypesForm(FlaskForm):
-    submit = SubmitField("Show Item Types")
