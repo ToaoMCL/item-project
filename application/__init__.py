9aa5b554a1ae7c2bdc8f5980ea5b747bf6027172
Eryk Pabianczyk <erykmcl@gmail.com>
2021-03-16 16:12:04 +0000

    I am unsure what but its a small change

--- application/__init__.py
+++ application/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db" #"mysql+pymysql://root:19BtnEmB8NBn6Jpo@35.234.157.160/world"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = "your key here"
 
 db = SQLAlchemy(app)
