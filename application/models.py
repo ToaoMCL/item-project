ea01f18316104af261c40285ea428e71501f2b42
Eryk Pabianczyk <erykmcl@gmail.com>
2021-03-16 16:04:17 +0000

    added a cascade to relationships allowing you to remove items and any dependants

--- application/models.py
+++ application/models.py
@@ -5,19 +5,19 @@ class Attributes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     description = db.Column(db.String(500), nullable=False)
-    items = db.relationship("ItemAttributes", backref="attributes")
+    items = db.relationship("ItemAttributes", backref="attributes", cascade="all,delete")
 
 class ItemTypes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(25), nullable=False)
-    items = db.relationship("Items", backref="item_types")
+    items = db.relationship("Items", backref="item_types", cascade="all,delete")
 
 class Items(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(30), nullable=False)
     fk_item_type = db.Column(db.Integer, db.ForeignKey(
         "item_types.id"), nullable=False)
-    items = db.relationship("ItemAttributes", backref="items")
+    items = db.relationship("ItemAttributes", backref="items",cascade="all,delete")
 
 class ItemAttributes(db.Model):
     id = db.Column(db.Integer, primary_key=True)
